352e32cdcb207ae323ef1aa5cd0489ee9f404b50
dilrong <dilrong@dilrong.com>
2021-11-08 01:23:00 +0900

    style: 디자인 일부추가
    - 레이아웃 컴포넌트 디자인
    - art 디자인
    - pfp 일부 디자인
    - css 변수 설정

--- src/view/pfp/AddByMinter.ts
+++ src/view/pfp/AddByMinter.ts
@@ -3,27 +3,39 @@ import { View, ViewParams } from "skyrouter";
 import Layout from "../Layout";
 
 export default class AddByMinter implements View {
+  private container: DomNode;
 
-    private container: DomNode;
+  constructor() {
+    Layout.current.title = "Minter로부터 PFP 등록";
+    Layout.current.content.append(
+      (this.container = el(
+        ".add-pfp-by-minter-view",
 
-    constructor() {
-        Layout.current.title = "Minter로부터 PFP 등록";
-        Layout.current.content.append(this.container = el(".add-pfp-by-minter-view",
-            el("h1", "Minter로부터 PFP 등록"),
-            el("p", "반드시 KIP17Mintable을 상속한 PFP여야만 합니다. 그리고 등록자는 Minter중 하나여야 합니다."),
-            el(".form",
-                el("label",
-                    el("span", "계약 주소"),
-                    el("input", { placeholder: "계약 주소" }),
-                ),
-                el("a", "등록하기"),
+        el("header.head", el("p.slogan", "Minter로부터 PFP 등록")),
+
+        el(
+          ".content",
+          el(
+            "p",
+            "반드시 KIP17Mintable을 상속한 PFP여야만 합니다. 그리고 등록자는 Minter중 하나여야 합니다."
+          ),
+          el(
+            ".form",
+            el(
+              "label",
+              el("span", "계약 주소"),
+              el("input", { placeholder: "계약 주소" })
             ),
-        ));
-    }
+            el("a", "등록하기")
+          )
+        )
+      ))
+    );
+  }
 
-    public changeParams(params: ViewParams, uri: string): void { }
+  public changeParams(params: ViewParams, uri: string): void {}
 
-    public close(): void {
-        this.container.delete();
-    }
+  public close(): void {
+    this.container.delete();
+  }
 }
