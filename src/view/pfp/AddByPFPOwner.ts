355fe1566776d02571e782e4b3285fb86b3b1177
Hanul <hanul@hanul.me>
2021-11-12 16:34:34 +0900

    Detail, Update 틀 작성

--- src/view/pfp/AddByPFPOwner.ts
+++ src/view/pfp/AddByPFPOwner.ts
@@ -4,57 +4,48 @@ import PFPsContract from "../../contracts/PFPsContract";
 import Layout from "../Layout";
 
 export default class AddByPFPOwner implements View {
-  private container: DomNode;
-  private input: DomNode<HTMLInputElement>;
+    private container: DomNode;
+    private input: DomNode<HTMLInputElement>;
 
-  constructor() {
-    Layout.current.title = "Owner로부터 PFP 등록";
-    Layout.current.content.append(
-      (this.container = el(
-        ".add-pfp-by-owner-view",
-
-        el("header.head", el("p.title", "PFP 프로젝트 등록"), el("p.subtitle", "Ownable을 상속한 PFP등록")),
-
-        el(
-          ".content",
-          el(
-            "ul",
-            el(
-              "li",
-              el(
-                ".form",
-                el(
-                  ".label",
-                  el("h6", "계약 주소"),
-                  this.input = el("input", { placeholder: "계약 주소" })
+    constructor() {
+        Layout.current.title = "Owner로부터 PFP 등록";
+        Layout.current.content.append(
+            (this.container = el(".add-pfp-by-owner-view",
+                el("header.head", el("p.title", "PFP 프로젝트 등록"), el("p.subtitle", "Ownable을 상속한 PFP등록")),
+                el(".content",
+                    el("ul",
+                        el("li",
+                            el(".form",
+                                el(".label",
+                                    el("h6", "계약 주소"),
+                                    this.input = el("input", { placeholder: "계약 주소" }),
+                                ),
+                                el("button.button-contained", "등록하기", {
+                                    click: async () => {
+                                        const addr = this.input.domElement.value;
+                                        const added = await PFPsContract.added(addr);
+                                        if (added === true) {
+                                            alert("해당 계약은 이미 정보가 등록되어있습니다.");
+                                        } else {
+                                            await PFPsContract.addByPFPOwner(addr);
+                                            setTimeout(() => alert("계약 정보 등록이 완료되었습니다.\n11일부터 해당 NFT는 거래를 할 수 있게됩니다.\nKlubs에 오신 것을 환영합니다."), 2000);
+                                        }
+                                    },
+                                }),
+                            ),
+                            el("p.danger-caption",
+                                "반드시 Ownable을 상속한 PFP여야만 합니다. 그리고 등록자는 반드시 Owner여야 합니다.",
+                            ),
+                        ),
+                    ),
                 ),
-                el("button.button-contained", "등록하기", {
-                  click: async () => {
-                    const addr = this.input.domElement.value;
-                    const added = await PFPsContract.added(addr);
-                    if (added === true) {
-                      alert("해당 계약은 이미 정보가 등록되어있습니다.");
-                    } else {
-                      await PFPsContract.addByPFPOwner(addr);
-                      setTimeout(() => alert("계약 정보 등록이 완료되었습니다.\n11일부터 해당 NFT는 거래를 할 수 있게됩니다.\nKlubs에 오신 것을 환영합니다."), 2000);
-                    }
-                  },
-                })
-              ),
-              el(
-                "p.danger-caption",
-                "반드시 Ownable을 상속한 PFP여야만 합니다. 그리고 등록자는 반드시 Owner여야 합니다."
-              )
-            )
-          )
-        )
-      ))
-    );
-  }
+            )),
+        );
+    }
 
-  public changeParams(params: ViewParams, uri: string): void { }
+    public changeParams(params: ViewParams, uri: string): void { }
 
-  public close(): void {
-    this.container.delete();
-  }
+    public close(): void {
+        this.container.delete();
+    }
 }
--- src/view/pfp/Update.ts
+++ src/view/pfp/Update.ts
@@ -5,11 +5,60 @@ import Layout from "../Layout";
 export default class Update implements View {
 
     private container: DomNode;
+    private bannerInput: DomNode<HTMLInputElement>;
+    private iconInput: DomNode<HTMLInputElement>;
+    private nameInput: DomNode<HTMLInputElement>;
+    private descriptionInput: DomNode<HTMLInputElement>;
+    private twitterInput: DomNode<HTMLInputElement>;
+    private kakaotalkInput: DomNode<HTMLInputElement>;
 
     constructor() {
         Layout.current.title = "PFP 정보 수정";
         Layout.current.content.append(this.container = el(".pfp-update-view",
             el("h1", "PFP 정보 수정"),
+            el(".form",
+                el(".banner-preview"),
+                el("label",
+                    "배너 업로드",
+                    this.bannerInput = el("input", { type: "file" }),
+                ),
+                el(".icon-preview"),
+                el("label",
+                    "아이콘 업로드",
+                    this.iconInput = el("input", { type: "file" }),
+                ),
+                el("label",
+                    "이름",
+                    this.nameInput = el("input", { type: "file" }),
+                ),
+                el("label",
+                    "소개글",
+                    this.descriptionInput = el("input", { type: "file" }),
+                ),
+                el("label",
+                    "오픈 카카오톡",
+                    this.kakaotalkInput = el("input", { type: "file" }),
+                ),
+                el("label",
+                    "트위터",
+                    this.twitterInput = el("input", { type: "file" }),
+                ),
+                el("a", "정보 저장"),
+            ),
+            el(".manage-managers",
+                el("h2", "매니저 관리"),
+                el("ul",
+                    el("li",
+                        el("span", "매니저 1"),
+                        el("a", "삭제"),
+                    ),
+                    el("li",
+                        el("span", "매니저 2"),
+                        el("a", "삭제"),
+                    ),
+                ),
+                el("a", "매니저 추가"),
+            ),
         ));
     }
 
