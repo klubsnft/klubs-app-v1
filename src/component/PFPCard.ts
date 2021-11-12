5907a705c0f371e0ac25472bccd8d805091fdd60
dilrong <dilrong@dilrong.com>
2021-11-12 20:56:48 +0900

    style: pfp-card 스타일링 외
    height: 50vh > 100%

--- src/component/PFPCard.ts
+++ src/component/PFPCard.ts
@@ -10,13 +10,13 @@ export default class PFPCard extends DomNode {
         name: string | undefined,
         description: string | undefined,
     ) {
-        super(".card-view");
+        super(".PFPCard");
 
         this.append(
-            el("img.banner", { src: banner, width: "200", height: "200" }),
-            el("img.icon", { src: icon, width: "200", height: "200" }),
+            el("img.banner", { src: banner }),
+            el("img.icon", { src: icon }),
             el(".name", name),
-            el(".description", description),
+            el(".description", description)
         );
 
         this.onDom("click", () => ViewUtil.go(`/pfp/${addr}`));
