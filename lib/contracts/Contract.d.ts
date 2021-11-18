933d92b13a97c9ab7bac662c77752359019f03d7
Hanul <hanul@hanul.me>
2021-11-18 20:28:35 +0900

    Klip 트랜잭션 처리

--- lib/contracts/Contract.d.ts
+++ lib/contracts/Contract.d.ts
@@ -4,12 +4,14 @@ export default abstract class Contract extends EventContainer {
     address: string;
     private abi;
     private walletContract;
-    private contract;
+    protected contract: any;
     constructor(address: string, abi: any);
     private findMethodABI;
     loadExtWalletContract(): Promise<any>;
     protected runMethod(methodName: string, ...params: any[]): Promise<any>;
-    protected runWalletMethod(methodName: string, ...params: any[]): Promise<unknown>;
-    protected runWalletMethodWithValue(value: BigNumber, methodName: string, ...params: any[]): Promise<unknown>;
+    private runWalletMethodWithGas;
+    protected runWalletMethod(methodName: string, ...params: any[]): Promise<void>;
+    protected runWalletMethodWithLargeGas(methodName: string, ...params: any[]): Promise<void>;
+    protected runWalletMethodWithValue(value: BigNumber, methodName: string, ...params: any[]): Promise<void>;
 }
 //# sourceMappingURL=Contract.d.ts.map
--- src/contracts/Contract.ts
+++ src/contracts/Contract.ts
@@ -1,13 +1,16 @@
 import { BigNumber } from "@ethersproject/bignumber";
 import EventContainer from "eventcontainer";
+import ConnectWalletPopup from "../component/ConnectWalletPopup";
+import Config from "../Config";
 import ExtWallet from "../klaytn/ExtWallet";
 import Klaytn from "../klaytn/Klaytn";
+import Klip from "../klaytn/Klip";
 import Wallet from "../klaytn/Wallet";
 
 export default abstract class Contract extends EventContainer {
 
     private walletContract: any | undefined;
-    private contract: any;
+    protected contract: any;
 
     constructor(public address: string, private abi: any) {
         super();
@@ -19,7 +22,7 @@ export default abstract class Contract extends EventContainer {
     }
 
     public async loadExtWalletContract() {
-        if (await ExtWallet.loadChainId() !== 8217) {
+        if (await ExtWallet.loadChainId() !== Config.chainId) {
             this.fireEvent("wrongNetwork");
             console.error("Wrong Network");
         } else {
@@ -37,15 +40,24 @@ export default abstract class Contract extends EventContainer {
         return await this.contract.methods[methodName](...params).call();
     }
 
-    protected async runWalletMethod(methodName: string, ...params: any[]) {
+    private async runWalletMethodWithGas(methodName: string, gas: number, ...params: any[]) {
         if (ExtWallet.installed === true) {
             const from = await Wallet.loadAddress();
             const contract = await this.loadExtWalletContract();
-            await contract?.methods[methodName](...params).send({ from, gas: 1500000 });
+            await contract?.methods[methodName](...params).send({ from, gas });
+        } else if (Klip.connected === true) {
+            await Klip.runContractMethod(this.address, this.findMethodABI(methodName), params);
         } else {
-            alert("카이카스가 필요합니다. 카이카스를 설치해주시기 바랍니다.");
-            return new Promise(() => { });
+            return new Promise<void>((resolve) => new ConnectWalletPopup(resolve));
         }
+    }
+
+    protected async runWalletMethod(methodName: string, ...params: any[]) {
+        return this.runWalletMethodWithGas(methodName, 1500000, ...params);
+    }
+
+    protected async runWalletMethodWithLargeGas(methodName: string, ...params: any[]) {
+        return this.runWalletMethodWithGas(methodName, 20000000, ...params);
     }
 
     protected async runWalletMethodWithValue(value: BigNumber, methodName: string, ...params: any[]) {
@@ -53,9 +65,10 @@ export default abstract class Contract extends EventContainer {
             const from = await Wallet.loadAddress();
             const contract = await this.loadExtWalletContract();
             await contract?.methods[methodName](...params).send({ from, gas: 1500000, value });
+        } else if (Klip.connected === true) {
+            await Klip.runContractMethod(this.address, this.findMethodABI(methodName), params, value.toString());
         } else {
-            alert("카이카스가 필요합니다. 카이카스를 설치해주시기 바랍니다.");
-            return new Promise(() => { });
+            return new Promise<void>((resolve) => new ConnectWalletPopup(resolve));
         }
     }
 }
