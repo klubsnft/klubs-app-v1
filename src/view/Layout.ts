64e0a6cd7b4aedbbf7efe970e5471d728f61d48d
dilrong <dilrong@dilrong.com>
2021-11-24 00:19:08 +0900

    style: footer sns 추가

--- src/view/Layout.ts
+++ src/view/Layout.ts
@@ -34,7 +34,7 @@ export default class Layout implements View {
                     ),
                 ),
                 el("main", (this.content = el(".content"))),
-                el("footer", "Copyright © 2021 Klubs. All rights reserved.")
+                el("footer", "Copyright © 2021 Klubs. All rights reserved.", el(".sns", el("a", { href: "https://discord.gg/mPanAs3s4w" }, el("img", { src: '/images/icon/discord-footer.svg' })), el("a", { href: "https://open.kakao.com/o/gfsahfHd" }, el("img", { src: "/images/icon/kakao-talk-footer.svg" }))))
             ))
         );
     }
