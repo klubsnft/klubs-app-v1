0280458f5500055d461a30710719c3906466268a
dilrong <dilrong@dilrong.com>
2022-03-06 17:13:20 +0900

    fix: loadTrade art seller

--- src/view/arts/ArtDetail.ts
+++ src/view/arts/ArtDetail.ts
@@ -121,9 +121,16 @@ export default class ArtDetail implements View {
                         click: () => ViewUtil.go(`/user/${saleInfo.seller}`),
                     }));
                 } else {
+                    let seller: string;
                     const auction = await ArtStoreContract.auctions(id);
-                    this.ownerDisplay.append(el("a", CommonUtil.shortenAddress(auction.seller), {
-                        click: () => ViewUtil.go(`/user/${auction.seller}`),
+                    if (auction.seller === "0x0000000000000000000000000000000000000000") {
+                        seller = ArtStoreContract.address;
+                    } else {
+                        seller = auction.seller;
+                    }
+
+                    this.ownerDisplay.append(el("a", CommonUtil.shortenAddress(seller), {
+                        click: () => ViewUtil.go(`/user/${seller}`),
                     }));
                 }
             } else {
