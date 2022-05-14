96c6a6b7c254a26b39f0fec41fddc1696ebb4e24
Sim Young Jae <hanul@hanul.me>
2022-05-14 22:52:11 +0900

    dev

--- src/component/NFTDisplay.ts
+++ src/component/NFTDisplay.ts
@@ -14,24 +14,25 @@ export default class NFTDisplay extends DomNode {
                 video.domElement.loop = true;
                 video.domElement.play();
             }
-        }
-        const extname = src.substring(src.lastIndexOf(".") + 1);
-        const contentType = (CONTENT_TYPES as any)[extname];
-        if (contentType === undefined || (
-            contentType.indexOf("image") === 0 &&
-            contentType.indexOf("image/svg") === -1
-        )) {
-            this.append(el("img", { src: thumbnail === true ? `https://api.klu.bs/thumbnail?url=${encodeURIComponent(src)}` : src }));
-        } else if (contentType.indexOf("image/svg") === 0) {
-            this.append(el("img", { src }));
-        } else if (contentType.indexOf("video") === 0) {
-            if (thumbnail === true) {
-                el("video", { src }).appendTo(this);
-            } else {
-                const video: DomNode<HTMLVideoElement> = el("video", { src }).appendTo(this) as any;
-                video.domElement.muted = true;
-                video.domElement.loop = true;
-                video.domElement.play();
+        } else {
+            const extname = src.substring(src.lastIndexOf(".") + 1);
+            const contentType = (CONTENT_TYPES as any)[extname];
+            if (contentType === undefined || (
+                contentType.indexOf("image") === 0 &&
+                contentType.indexOf("image/svg") === -1
+            )) {
+                this.append(el("img", { src: thumbnail === true ? `https://api.klu.bs/thumbnail?url=${encodeURIComponent(src)}` : src }));
+            } else if (contentType.indexOf("image/svg") === 0) {
+                this.append(el("img", { src }));
+            } else if (contentType.indexOf("video") === 0) {
+                if (thumbnail === true) {
+                    el("video", { src }).appendTo(this);
+                } else {
+                    const video: DomNode<HTMLVideoElement> = el("video", { src }).appendTo(this) as any;
+                    video.domElement.muted = true;
+                    video.domElement.loop = true;
+                    video.domElement.play();
+                }
             }
         }
     }
