dc8c388ef0971330c9061c0284b0eb8c8be645b1
Sim Young Jae <hanul@hanul.me>
2022-05-18 21:09:28 +0900

    dev

--- lib/component/NFTDisplay.d.ts
+++ lib/component/NFTDisplay.d.ts
@@ -1,5 +1,5 @@
 import { DomNode } from "@hanul/skynode";
 export default class NFTDisplay extends DomNode {
-    constructor(src: string, thumbnail?: boolean);
+    constructor(src: string, thumbnail?: boolean, video?: boolean);
 }
 //# sourceMappingURL=NFTDisplay.d.ts.map
